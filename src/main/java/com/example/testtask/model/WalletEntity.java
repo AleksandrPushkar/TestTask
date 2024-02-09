b448158867cd474c43e69ff0aa4cde581d362ac8
Aleksandr Pushkar <aleksandr.pushkar94@mail.ru>
2024-02-09 21:32:05 +0300

    In file pom.xml added luquibase dependency. Added liquibase configuration file  and changeset filles for created table wallets and added three rows

--- src/main/java/com/example/testtask/model/WalletEntity.java
+++ src/main/java/com/example/testtask/model/WalletEntity.java
@@ -2,10 +2,12 @@ package com.example.testtask.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "wallets")
 public class WalletEntity {
     @Id
@@ -13,5 +15,4 @@ public class WalletEntity {
     private Integer id;
 
     private Integer balance;
-
 }
