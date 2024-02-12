3035f700fbe11ea7601364570090494d3fc68773
aleksandrPushkar <aleksandr.pushkar94@mail.ru>
2024-02-12 07:47:44 +0300

    Adding method getBalanceTest which for testing

--- src/test/java/com/example/testtask/ApiControllerTest.java
+++ src/test/java/com/example/testtask/ApiControllerTest.java
@@ -11,7 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +32,8 @@ class ApiControllerTest {
 
     @Test
     void getBalance() throws Exception{
-        mockMvc.perform(get("/api/v1/wallets/")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/wallets/{id}", "614126b2-a3ff-4152-96c2-19546d316671"))
+                .andExpect(status().isOk())
+                .andExpect(("$").value(10000));
     }
 }
